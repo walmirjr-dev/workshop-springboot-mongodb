3611ee61533f0ea49a63327f6f3c8fd0acb410ac
Walmir Alves <walmirjr915@gmail.com>
2025-09-16 19:42:22 -0300

    User and UserResource initial implementation

---
+++ src/main/java/com/walmiralves/workshopdemo/resources/UserResource.java
@@ -0,0 +1,29 @@
+package com.walmiralves.workshopdemo.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.walmiralves.workshopdemo.domain.User;
+
+@RestController
+@RequestMapping(value ="/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		User maria = new User("1", "Maria Silva", "maria@gmail.com");
+		User alex = new User("2", "Alex Silva", "Alex@gmail.com");
+
+		List<User> list = new ArrayList<>();
+
+		list.addAll(Arrays.asList(maria, alex));
+
+		return ResponseEntity.ok().body(list);
+	}
+}
