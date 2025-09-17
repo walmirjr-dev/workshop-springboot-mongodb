2a4468d67766cbaf792b36260a812a574d3bbacb
Walmir Alves <walmirjr915@gmail.com>
2025-09-16 22:33:08 -0300

    Getting user by ID

--- src/main/java/com/walmiralves/workshopdemo/services/UserService.java
+++ src/main/java/com/walmiralves/workshopdemo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.walmiralves.workshopdemo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.walmiralves.workshopdemo.domain.User;
 import com.walmiralves.workshopdemo.repositories.UserRepository;
+import com.walmiralves.workshopdemo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,8 +19,13 @@ public class UserService {
 	public List<User> findAll(){
 		return repository.findAll();
 	}
-	
-	
+
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+
+
 
 
 }
