1e8c36cdc9b5b9ad3dfdfaa3138618cb89f65256
Walmir Alves <walmirjr915@gmail.com>
2025-09-17 18:11:03 -0300

    Endpoint to get user posts

--- src/main/java/com/walmiralves/workshopdemo/resources/UserResource.java
+++ src/main/java/com/walmiralves/workshopdemo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.walmiralves.workshopdemo.domain.Post;
 import com.walmiralves.workshopdemo.domain.User;
 import com.walmiralves.workshopdemo.dto.UserDTO;
 import com.walmiralves.workshopdemo.services.UserService;
@@ -61,6 +62,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping(value = "{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 
 
