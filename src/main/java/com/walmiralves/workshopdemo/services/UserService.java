d8ff0fb0bdfd4cde9ca41164bfa791db67750929
Walmir Alves <walmirjr915@gmail.com>
2025-09-16 22:40:02 -0300

    User insertion with POST method

--- src/main/java/com/walmiralves/workshopdemo/services/UserService.java
+++ src/main/java/com/walmiralves/workshopdemo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.walmiralves.workshopdemo.domain.User;
+import com.walmiralves.workshopdemo.dto.UserDTO;
 import com.walmiralves.workshopdemo.repositories.UserRepository;
 import com.walmiralves.workshopdemo.services.exceptions.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 
