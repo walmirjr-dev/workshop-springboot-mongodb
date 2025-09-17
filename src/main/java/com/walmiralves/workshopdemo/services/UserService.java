0b27237bb5e0396b8cabd6bb9754aab113bf2fc6
Walmir Alves <walmirjr915@gmail.com>
2025-09-16 23:00:14 -0300

    User update using PUT method

--- src/main/java/com/walmiralves/workshopdemo/services/UserService.java
+++ src/main/java/com/walmiralves/workshopdemo/services/UserService.java
@@ -39,6 +39,17 @@ public class UserService {
 		repository.deleteById(id);
 	}
 
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 
 
 
